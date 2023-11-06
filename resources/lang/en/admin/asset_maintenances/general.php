019dce03d1ee41b31ac607242999be2606ba0906
Darth Killian <rhuntington719@gmail.com>
2023-11-06 15:41:05 -0600

    3rd times the charm.... fixed spelling mistake
    Signed-off-by: Darth Killian <rhuntington719@gmail.com>

--- resources/lang/en/admin/asset_maintenances/general.php
+++ resources/lang/en/admin/asset_maintenances/general.php
@@ -6,7 +6,7 @@
         'delete'             => 'Delete Asset Maintenance',
         'view'               => 'View Asset Maintenance Details',
         'eti_repair'         => 'ETI Repair',
-        'waranty_repair'     => 'Warranty RMA',
+        'warranty_repair'     => 'Warranty RMA',
         'oow_repair'         => 'OOW Repair',
         'ar'                 => 'Advanced Replacement',
     ];
