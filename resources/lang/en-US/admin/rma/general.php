df8e186a26bebda45c223f4e6c80fefc76c03850
DarthKillian <rhuntington719@gmail.com>
2024-02-08 18:45:41 -0600

    Fix missing language reference for RMA... 3rd times the charm??
    Signed-off-by: DarthKillian <rhuntington719@gmail.com>

--- resources/lang/en-US/admin/rma/general.php
+++ resources/lang/en-US/admin/rma/general.php
@@ -19,4 +19,6 @@ return [
     'updated_at' => 'Updated At',
     'view' => 'View RMA for ',
     'edit' => 'Edit',
+    'rma_notify' => 'Notify Admin',
+    'rma_notify_help' => 'Send an email notification to the front office'
 ];
